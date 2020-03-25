e15469486be038e0faeed09a82d44828299be429
Rodrigo Quesada <rodrigoquesada.dev@gmail.com>
2020-03-24 19:18:50 -0600

    Added 'useCustomMemo', 'useDistinct' and 'useFirst' hooks.

---
+++ src/general/use-custom-memo.ts
@@ -0,0 +1,33 @@
+import {DependencyList, useMemo} from "react";
+import fastDeepEqual from "fast-deep-equal/react";
+import {groupBy, merge} from 'lodash-es';
+import {useDistinct} from "./use-distinct";
+import {useFirst} from "./use-first";
+
+type DepsComparison = (prev: DependencyList, next: DependencyList) => boolean;
+
+type UseCustomMemoOptions = {
+    comparison: DepsComparison,
+    useShallowComparison: (dep: any) => boolean
+};
+
+const DONT_USE_SHALLOW_COMPARISON = () => false;
+
+export function useCustomMemo<T>(
+    factory: () => T,
+    deps: DependencyList,
+    options?: Partial<UseCustomMemoOptions>
+): T
+{
+    options = useFirst(options);
+
+    const {comparison, useShallowComparison}: UseCustomMemoOptions = merge({
+        comparison: fastDeepEqual,
+        useShallowComparison: DONT_USE_SHALLOW_COMPARISON
+    }, options);
+    const {'true': shallowComparisonDeps = [], 'false': customComparisonDeps = []} = groupBy(deps, useShallowComparison);
+
+    const prevCustomComparisonDeps = useDistinct<DependencyList>(customComparisonDeps, {comparison});
+
+    return useMemo(factory, [...shallowComparisonDeps, prevCustomComparisonDeps]);
+}
